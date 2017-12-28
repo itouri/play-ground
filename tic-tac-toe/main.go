322054a774f76887da1c732a460616cccaf114ca
akiyama_k <hechima.itouri@gmail.com>
2017-12-28 16:55:36 +0900

    make printf

--- tic-tac-toe/main.go
+++ tic-tac-toe/main.go
@@ -4,6 +4,44 @@ import (
 	"fmt"
 )
 
+type EnumMark int
+
+// Request methods
+const (
+	Black EnumMark = iota
+	White
+	None
+)
+
+func printField(field [3][3]EnumMark) {
+	for _, f := range field {
+		for _, m := range f {
+			switch EnumMark(m) {
+			case Black:
+				fmt.Printf("o")
+			case White:
+				fmt.Printf("x")
+			case None:
+				fmt.Printf("-")
+			}
+		}
+		fmt.Printf("\n")
+	}
+}
+
+func initField() [3][3]EnumMark {
+	var field [3][3]EnumMark
+	for x, f := range field {
+		for y := range f {
+			field[x][y] = None
+		}
+	}
+	return field
+}
+
 func main() {
 	fmt.Printf("Tic Tac Toe\n")
+	var field [3][3]EnumMark
+	field = initField()
+	printField(field)
 }
