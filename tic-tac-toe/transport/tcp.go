13e35738a3eb4aec7de7ed7a99fcf2c5b4996a36
itouri <k-akiyama@axsh.net>
2017-12-30 22:40:50 +0900

    Add some functions

--- tic-tac-toe/transport/tcp.go
+++ tic-tac-toe/transport/tcp.go
@@ -1,14 +1,17 @@
 package transport
 
 import (
+	"fmt"
+	"net"
+
 	"../packet"
 )
 
 type Tcp struct {
 	// // リスニングソケット.
-	// private Socket			m_listener = null;
+	listener *net.TCPListener
 	// // クライアントとの接続用ソケット.
-	// private Socket			m_socket = null;
+	socket net.Conn
 
 	sendQueue   packet.Queue
 	recvQueue   packet.Queue
@@ -32,10 +35,57 @@ func (t *Tcp) NewTcp() {
 	t.isConnected = false
 }
 
+func (t *Tcp) StartServer(port int, connNum int) bool {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", ":"+PORT)
+	failOnError(err)
+
+	t.listener, err := net.ListenTCP("tcp", tcpAddr)
+	failOnError(err)
+	defer t.listener.Close()
+
+	t.isServer = true
+
+	// for <-end {
+	for {
+		t.socket, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+		go handleClient(conn)
+	}
+}
+
+func (t *Tcp) handleClient() {
+	// クライアントとの小受信を処理します.
+	if (m_socket != null && m_isConnected == true) {
+		// 送信処理.
+		dispatchSend();
+		// 受信処理.
+		dispatchReceive();
+	}
+}
+
+
+func (t *Tcp) dispatchSend() {
+	t.socket.
+}
+
+func (t *Tcp) dispatchReceive() {
+	
+}
+
 func (t *Tcp) IsServer() {
 	return t.isServer
 }
 
 func (t *Tcp) IsConnected() {
 	return t.isConnected
+}
+
+func (t *Tcp) acceptClient() {
+	// if t.listener != nil && t.listener.Poll(0, SelectMode.SelectRead) {
+	if t.listener != nil {
+		t.socket = t.listener.Accept()
+		t.isConnected = true
+	}
 }
