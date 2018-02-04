a3eae818abe8c5fb927181fb911d965b17a654b5
akiyama_k <hechima.itouri@gmail.com>
2018-02-04 15:59:26 +0900

    Modified chanchan to using wait group

--- parallel-pattern/chanchan.go
+++ parallel-pattern/chanchan.go
@@ -4,38 +4,37 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-func do(numChans chan chan int, ch chan int, n int, wait time.Duration) {
+func do(wg *sync.WaitGroup, numChans chan chan int, ch chan int, n int, wait time.Duration) {
 	numChans <- ch
 	time.Sleep(wait * time.Second)
 	fmt.Printf("end sleep: %d\n", n)
 	ch <- n
+	wg.Done()
 }
 
 func main() {
 	// バッファ付きにするの忘れてしばらく詰まった
 	numChans := make(chan chan int, 3)
-	end := make(chan bool)
+	var wg sync.WaitGroup
+	wg.Add(3)
 
 	ch1 := make(chan int)
 	ch2 := make(chan int)
 	ch3 := make(chan int)
 
-	go do(numChans, ch1, 1, 3)
-	go do(numChans, ch2, 2, 2)
-	go do(numChans, ch3, 3, 1)
+	go do(&wg, numChans, ch1, 1, 3)
+	go do(&wg, numChans, ch2, 2, 2)
+	go do(&wg, numChans, ch3, 3, 1)
 
-	go func(end chan bool) {
+	go func() {
 		for numChan := range numChans {
 			num := <-numChan
 			fmt.Printf("main: %d\n", num)
-			if num == 3 {
-				end <- true
-			}
 		}
-	}(end)
-
-	<-end
+	}()
+	wg.Wait()
 }
