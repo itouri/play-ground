f96f3b19c6d87cbde896f79f23e4b30c4884a93a
akiyama_k <hechima.itouri@gmail.com>
2018-02-04 23:37:19 +0900

    Added not compele reactiveX.go

---
+++ parallel-pattern/reactiveX.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/reactivex/rxgo/observable"
+	"github.com/reactivex/rxgo/observer"
+)
+
+func main() {
+	// observableを作成
+	emitter := make(chan interface{})
+	source := observable.Observable(emitter)
+
+	watcher := observer.Observer{}
+}
