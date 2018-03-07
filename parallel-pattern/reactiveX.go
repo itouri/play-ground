c5aba525bc67a11f85fd4acdfc54a288062c4f9d
akiyama_k <hechima.itouri@gmail.com>
2018-03-07 23:20:14 +0900

    Add reactive

--- parallel-pattern/reactiveX.go
+++ parallel-pattern/reactiveX.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+
 	"github.com/reactivex/rxgo/observable"
 	"github.com/reactivex/rxgo/observer"
 )
@@ -8,9 +12,37 @@ import (
 func main() {
 	// observableを作成
 	emitter := make(chan interface{})
+
 	source := observable.Observable(emitter)
 
 	watcher := observer.Observer{
-		
+		NextHandler: func(item interface{}) {
+			line := item.(string)
+			if strings.HasPrefix(line, "func ") {
+				fmt.Println(line)
+			}
+		},
+		ErrHandler: func(err error) {
+			fmt.Printf("error: %v\n", err)
+		},
+		DoneHandler: func() {
+			fmt.Println("Done!")
+		},
 	}
+
+	// Observableに振る舞いを設定
+	sub := source.Subscribe(watcher)
+
+	go func() {
+		content, err := ioutil.ReadFile("chanchan.go")
+		if err != nil {
+			emitter <- err
+		} else {
+			for _, line := range strings.Split(string(content), "\n") {
+				emitter <- line
+			}
+		}
+		close(emitter)
+	}()
+	<-sub
 }
