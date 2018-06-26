db487ca4dfe9d7a0369c2b4560a124c12653f070
itouri <k-akiyama@axsh.net>
2018-06-26 14:05:26 +0900

    Separated to webgl.ts

--- webgl/own/main.ts
+++ webgl/own/main.ts
@@ -1,43 +1,6 @@
 import { MatIV } from './minMatrix';
+import { WebGL } from './webgl';
 
-function compileShader(gl : WebGLRenderingContext, shader : WebGLShader, code : string) : void {
-    gl.shaderSource(shader, code);
-    gl.compileShader(shader);
-
-    if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        console.log(gl.getShaderInfoLog(shader));
-        throw new Error('compile error');
-    }
-}
-
-function createProgram(gl : WebGLRenderingContext, vertexCode : string, fragmentCode : string) : WebGLProgram {
-    const vertexShader = gl.createShader(gl.VERTEX_SHADER)!;
-    compileShader(gl, vertexShader, vertexCode);
-
-    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)!;
-    compileShader(gl, fragmentShader, fragmentCode);
-
-    const program = gl.createProgram()!;
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-
-    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-        console.log(gl.getProgramInfoLog(program));
-        throw new Error("program error");
-    }
-    return program;
-}
-
-function createVertexBuffer(gl : WebGLRenderingContext, vertices : number[]) : WebGLBuffer {
-    const buffer = gl.createBuffer()!;
-
-    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    gl.bindBuffer(gl.ARRAY_BUFFER, null); //TODO いらないの?
-
-    return buffer;
-}
 
 //TODO 型anyは良くない
 function createMatrix() : any {
@@ -54,7 +17,9 @@ function createMatrix() : any {
     
     // プロジェクション座標変換行列
     //TODO width, heightのハードコーディング
-    m.perspective(90, 300 / 300, 0.1, 100, pMatrix);
+    const width = 300;
+    const height = 300;
+    m.perspective(90, width / height, 0.1, 100, pMatrix);
     
     // 各行列を掛け合わせ座標変換行列を完成させる
     m.multiply(pMatrix, vMatrix, mvpMatrix);
@@ -63,22 +28,14 @@ function createMatrix() : any {
     return mvpMatrix;
 }
 
-function setAttribute(gl : WebGLRenderingContext, buffers : WebGLBuffer[], attLocations : number[], attStrides : number[] ) {
-    for(let i in buffers){
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffers[i]);        
-        gl.enableVertexAttribArray(attLocations[i]);
-        gl.vertexAttribPointer(attLocations[i], attStrides[i], gl.FLOAT, false, 0, 0);
-    }
-}
-
 function main() : void {
     const canvas = <HTMLCanvasElement>document.getElementById('canvas');
+    let context = canvas.getContext('webgl')!;
 
-    let gl : WebGLRenderingContext;
-    gl = canvas.getContext('webgl')!;
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    gl.clearDepth(1.0);
-    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
+    let webgl = new WebGL(context);
+    webgl.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    webgl.gl.clearDepth(1.0);
+    webgl.gl.clear( webgl.gl.COLOR_BUFFER_BIT | webgl.gl.DEPTH_BUFFER_BIT );
 
     const vertexCode = `
         attribute vec3 position;
@@ -95,7 +52,7 @@ function main() : void {
         }
     `;
 
-    const program = createProgram(gl, vertexCode, fragmentCode);
+    const program = webgl.createProgram(vertexCode, fragmentCode);
 
     const vertices = [
         0.0, 1.0, 0.0,
@@ -103,27 +60,27 @@ function main() : void {
         1.0, -1.0, 0.0
     ];
     const vertexBuffer = [
-        createVertexBuffer(gl, vertices),
+        webgl.createVertexBuffer(vertices),
     ];
    
     //TODO なんかそうじゃない
     let attLocations = [
-        gl.getAttribLocation(program, 'position'),
+        webgl.gl.getAttribLocation(program, 'position'),
     ];
     let attStrides = [3];
 
     //wgldではcreateProgram()の時点で行っている
-    gl.useProgram(program);
+    webgl.gl.useProgram(program);
 
-    setAttribute(gl, vertexBuffer, attLocations, attStrides)
+    webgl.setAttribute(vertexBuffer, attLocations, attStrides)
 
-    const uniLocation = gl.getUniformLocation(program, 'mvpMatrix');
+    const uniLocation = webgl.gl.getUniformLocation(program, 'mvpMatrix');
     const mvpMatrix = createMatrix();
-    gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+    webgl.gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
 
-    gl.clearColor(0.3, 0.3, 0.3, 1);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.TRIANGLES, 0, 3);
+    webgl.gl.clearColor(0.3, 0.3, 0.3, 1);
+    webgl.gl.clear(webgl.gl.COLOR_BUFFER_BIT);
+    webgl.gl.drawArrays(webgl.gl.TRIANGLES, 0, 3);
 }
 
 onload = function() {
