155ba6c65525ff59ddfa5b8cdfba1d903cb1ba3f
itouri <k-akiyama@axsh.net>
2018-09-04 18:58:34 +0900

    added undo

--- monopoly.py
+++ monopoly.py
@@ -1,7 +1,25 @@
+from copy import deepcopy
+
 _player_num = 0
 _player_name = []
 _player_vlaue = []
 _longest_name_len = 0
+
+_undo_player_vlaue = []
+
+def undo():
+    global _player_vlaue
+    global _undo_player_vlaue
+    if (len(_undo_player_vlaue) == 0):
+        print("undo only once")
+        return
+    _player_vlaue = deepcopy(_undo_player_vlaue)
+    _undo_player_vlaue = []
+    
+def push_pv():
+    global _player_vlaue
+    global _undo_player_vlaue
+    _undo_player_vlaue = deepcopy(_player_vlaue)
 
 def display_status():
     print()
@@ -20,21 +38,26 @@ def input_player_name():
         _player_name[i] = input()
 
 def game():
+    global _player_vlaue
     while True:
         a = []
         try:
             a = list(map(int, input().split()))
         except ValueError:
-            print("not invalid input")
+            print("invalid input")
             continue
         
-        if len(a) == 2:
+        if len(a) == 1 and a[0] == 0:
+            undo()
+        elif len(a) == 2:
+            push_pv()
             _player_vlaue[a[0]-1] += a[1]
         elif len(a) == 3:
+            push_pv()
             _player_vlaue[a[0]-1] -= a[2]
             _player_vlaue[a[1]-1] += a[2]
         else:
-            print("format is not valid")
+            print("invalid format")
             continue
         
         display_status()
